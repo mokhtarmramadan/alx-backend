927cebb1739b72f2c2bf0f3141a2ef5ae98fbebb
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-06-25 13:21:29 +0300

    06-25 13:21

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+''' FIFOCache class '''
+from base_class import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    ''' A caching system that follows FIFO pattern '''
+
+    def put(self, key, item):
+        ''' Assigns key to it's item in the chache dict
+            if data is more than the max_limit we pop
+            the first put element '''
+        if key is not None and item is not None:
+            if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                    key not in list(self.cache_data.keys())):
+                first_key = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_key)
+                print("DISCARD: ", first_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Gets a certain item by it's key '''
+        value = slef.cache_data.get(key, None)
+        return value
