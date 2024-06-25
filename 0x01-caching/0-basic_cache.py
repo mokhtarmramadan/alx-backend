3072926901d3cbded8af4b3dbda89848aca4dbc8
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-06-25 11:51:41 +0300

    06-25 11:51

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' BasicCache class '''
+from base_class import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' A caching system class '''
+
+    def put(self, key, item):
+        ''' Assign new data to the dictionary
+            if key or item don't exist, this method
+            will skip adding it '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Returns the value of a certain key in
+            the dict or None if there's not any '''
+        value = self.cache_data.get(key, None)
+        return value
