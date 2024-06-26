497484e855734d4be292ab06fc3cfee64d1ee821
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-06-26 15:40:53 +0300

    06-26 15:40

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,8 +5,8 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     ''' BaseCaching class '''
-    
-    elements_key = [] # recently used first
+
+    elements_key = []  # recently used first
 
     def put(self, key, item):
         ''' Assigns key to it's item in the chache dict
@@ -15,13 +15,20 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             discard_element = ''
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard_element = LRUCache.elements_key.pop(0)
-                del BaseCaching.cache_data[discard_element]
-                print("Discard:", discard_element)
+                if key not in list(self.cache_data.keys()):
+                    discard_element = LRUCache.elements_key.pop(0)
+                    del self.cache_data[discard_element]
+                    print("Discard:", discard_element)
+                else:
+                    LRUCache.elements_key.remove(key)
             self.cache_data[key] = item
             LRUCache.elements_key.append(key)
-    
+
     def get(self, key):
-        ''' Gets a certain item by it's key '''
+        ''' Gets a certain item by it's key and adds them in the
+        beginning fo the list as the were recently used '''
         value = self.cache_data.get(key, None)
+        if value is not None:
+            LRUCache.elements_key.remove(key)
+            LRUCache.elements_key.append(key)
         return value
