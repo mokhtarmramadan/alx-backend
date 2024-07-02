309d8c16c59ee63df890013bbad8263d64f8d322
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-07-02 13:35:52 +0300

    07-02 13:35

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
 ''' A basic Flask app that Create a single / route and an index.html '''
 
@@ -5,8 +6,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes=False)
+def index() -> str:
     ''' renders index.html template '''
     return render_template('index.html')
 
